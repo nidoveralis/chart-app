2cdccf7641221efea3648f5c09f2372dbd3ad7ff
Nidoveralis <nidoveralis@gmail.com>
2024-11-27 22:42:08 +0400

    feature: added chart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+} from "chart.js";
+
+import "./App.css";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+const isData = [
+  {
+    backgroundColor: "rgb(241 190 66)",
+    borderColor: "rgb(241 190 66)",
+    data: [0, 121, 101, 72, 93, 44, 63, 0, 23],
+    label: "Short",
+  }];
 
 function App() {
+  const data = {
+    labels: ["Атак", "Ашан", "Ашан-Сити", "Гиперглобус", "Конвент", "Лента", "Магнит ГМ", "ОКЕЙ", "Metro C&C"],
+    datasets: isData,
+  };
+
+  const options = {
+    responsive: true,
+    indexAxis: "x",
+    plugins: {
+      legend: {
+        display: true,
+        position: "bottom",
+        labels: {
+        },
+      },
+      datalabels: {
+        color: "#000",
+        align: "end",
+        anchor: "center",
+        formatter: (value) => { return value > 0 ? value : "" },
+        offset: -3,
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Line
+          data={data}
+          options={options}
+          style={{
+            cursor: "pointer",
+            maxWidth: "75vw",
+            maxHeight: "60vh"
+          }}
+        />
+      </div>
     </div>
   );
 }
